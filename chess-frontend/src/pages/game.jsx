3b9a8faf6b92ded1497b072cbde4aa100029b108
Saiteja4257 <saitejadasari1212@gmail.com>
2025-04-16 00:32:01 +0530

    GO

--- chess-frontend/src/pages/game.jsx
+++ chess-frontend/src/pages/game.jsx
@@ -9,13 +9,13 @@ const ChooseGameMode = () => {
       <h1 className="choose-title">♟️Choose Game Mode♟️</h1>
       <div className="button-wrapper">
         <button className="game-button"onClick={() => navigate('/chessAi')} >
-          <img src="https://static.vecteezy.com/system/resources/previews/019/814/601/non_2x/robot-vs-human-playing-chess-game-artificial-intelligence-technology-symbol-cartoon-illustration-vector.jpg" alt="Play vs AI" />
+          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/v1744743651/aichess3_eww93a.png" alt="Play vs AI" />
           <br></br>
           <h2 className="option">Play Vs AI</h2>
         </button>
         
         <button className="game-button" onClick={() => navigate('/maingame')}>
-          <img src="https://media.istockphoto.com/id/1222517294/vector/draw.jpg?s=612x612&w=0&k=20&c=oQW3Bf2COveSBKKCh0fn9N9RTp9ZaEXoEttl_SRoMDs=" alt="Play vs Friend" />
+          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/v1744743093/boychess_wi36yu.png" alt="Play vs Friend" />
           <br></br>
           <h2 className='option' >Play with Friend</h2>
         </button>
