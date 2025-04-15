5d94b89976750375b85d0e475e7754622b71eef2
Saiteja4257 <saitejadasari1212@gmail.com>
2025-04-16 00:36:36 +0530

    go1

--- chess-frontend/src/pages/game.jsx
+++ chess-frontend/src/pages/game.jsx
@@ -9,13 +9,13 @@ const ChooseGameMode = () => {
       <h1 className="choose-title">♟️Choose Game Mode♟️</h1>
       <div className="button-wrapper">
         <button className="game-button"onClick={() => navigate('/chessAi')} >
-          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/v1744743651/aichess3_eww93a.png" alt="Play vs AI" />
+          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/f_auto,q_auto/v1744743651/aichess3_eww93a.png" alt="Play vs AI" />
           <br></br>
           <h2 className="option">Play Vs AI</h2>
         </button>
         
         <button className="game-button" onClick={() => navigate('/maingame')}>
-          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/v1744743093/boychess_wi36yu.png" alt="Play vs Friend" />
+          <img src="https://res.cloudinary.com/dlbc9t2ft/image/upload/v1744743093/f_auto,q_auto/boychess_wi36yu.png" alt="Play vs Friend" />
           <br></br>
           <h2 className='option' >Play with Friend</h2>
         </button>
